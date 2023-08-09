1d9d6e965dd8f8190cd759245271e0d6ee89ef18
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-08 17:20:52 -0700

    Fixed up Hero section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const HeroSection = () => {
     return (
-        <div className="text-8xl font-bold flex items-center justify-center h-[70vh] custom-bg text-white">
-            <p className="bg-gray-900/90 p-4 rounded-sm">HAVE A TASTE OF MY MEAT</p>
+        <div>
+            <div className="text-8xl font-bold flex items-center justify-center h-[70vh] custom-bg text-white">
+                <p className="bg-gray-900/90 p-4 rounded-sm">
+                    HAVE A TASTE OF MY MEAT
+                </p>
+            </div>
+            <div>
+                <Link className="bg-white flex text-center justify-center font-bold hover:text-blue-900 p-4 rounded-sm" href="/collection" >
+                        CHECKOUT OUR COLLECTION  
+                </Link>
+            </div>
         </div>
     );
 };
