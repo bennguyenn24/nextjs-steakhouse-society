7504ee4633fd7bbf47ba5c51c3f634d15498669d
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-09 18:33:56 -0700

    Implemented collection page + routing

--- app/(products)/collection/page.jsx
+++ app/(products)/collection/page.jsx
@@ -1,39 +1,66 @@
-'use client'
+"use client";
 import Link from "next/link";
-// import steak from './public/steak.jpg'
-// import pork from "./public/pork.jpg";
-// import seafood from "./public/seafood.jpg";
+import Image from "next/image";
 
 const page = () => {
-    // const categories = [
-    //     {title: 'Steaks', image: '/steak.jpg', Link: '/steaks'}, 
-    //     {title: 'Pork', image: '/pork.jpg', Link: '/pork'}, 
-    //     {title: 'Seafood', image: '/.jpg', Link: '/seafood'} 
-    // ]
+    return (
+        <div className="flex flex-col items-center p-5">
+            <p className="text-4xl p-8">
+                Take a look at our extensive collection
+            </p>
 
-        return (
-            
-                <div className="flex justify-center">
-                  <div className="bg-white p-4 rounded-md shadow-md max-w-xs mx-2">
-                    <div className="w-full h-40 bg-gray-200 mb-4 rounded-md"></div>
-                    <h2 className="text-xl font-semibold mb-2">Product Title</h2>
-                    <p className="text-gray-600 mb-1">Category</p>
-                    <p className="text-blue-700 font-bold">Price</p>
-                  </div>
-                  <div className="bg-white p-4 rounded-md shadow-md max-w-xs mx-2">
-                    <div className="w-full h-40 bg-gray-200 mb-4 rounded-md"></div>
-                    <h2 className="text-xl font-semibold mb-2">Product Title</h2>
-                    <p className="text-gray-600 mb-1">Category</p>
-                    <p className="text-blue-700 font-bold">Price</p>
-                  </div>
-                  <div className="bg-white p-4 rounded-md shadow-md max-w-xs mx-2">
-                    <div className="w-full h-40 bg-gray-200 mb-4 rounded-md"></div>
-                    <h2 className="text-xl font-semibold mb-2">Product Title</h2>
-                    <p className="text-gray-600 mb-1">Category</p>
-                    <p className="text-blue-700 font-bold">Price</p>
-                  </div>
-                </div>
-        )
-      };
+            <div className="flex flex-col md:flex-row justify-center p-5">
+                <Link
+                    className="bg-white p-8 rounded-lg shadow-2xl max-w-sm mx-4 my-4 md:mx-10 md:my-8 hover:shadow-lg transition duration-300 transform hover:scale-105"
+                    href="/steaks"
+                >
+                    <div className="w-40 h-40 bg-gray-200 mb-4 rounded-md">
+                        <Image
+                            src="/steak.jpg"
+                            width={500}
+                            height={500}
+                           
+                            alt=""
+                            style={{ maxWidth: "100%", height: "100%" }}
+                        />
+                    </div>
+                    <h2 className="text-xl font-semibold mt-16">Steaks</h2>
+                </Link>
+                <Link
+                    className="bg-white p-8 rounded-lg shadow-2xl max-w-sm mx-4 my-4 md:mx-10 md:my-8 hover:shadow-lg transition duration-300 transform hover:scale-105"
+                    href="/pork"
+                >
+                    <div className="w-40 h-40 bg-gray-200 mb-4 rounded-md">
+                        <Image
+                            src="/porks.jpg"
+                            width={500}
+                            height={500}
+                            
+                            alt=""
+                            style={{ maxWidth: "100%", height: "100%" }}
+                        />
+                    </div>
+                    <h2 className="text-xl font-semibold mt-16">Pork</h2>
+                </Link>
+                <Link
+                    className="bg-white p-8 rounded-lg shadow-2xl max-w-sm mx-4 my-4 md:mx-10 md:my-8 hover:shadow-lg transition duration-300 transform hover:scale-105"
+                    href="/seafood"
+                >
+                    <div className="w-40 h-40 bg-gray-200 mb-4 rounded-md">
+                        <Image
+                            src="/seafood.jpg"
+                            width={500}
+                            height={500}
+                           
+                            alt=""
+                            style={{ maxWidth: "100%", height: "100%" }}
+                        />
+                    </div>
+                    <h2 className="text-xl font-semibold mt-16">Seafood</h2>
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default page;
