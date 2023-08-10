a04f9c4d8ec4285a3c23105fc2c28d215f65364c
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-10 14:39:01 -0700

    Implemented logo into navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import Link from 'next/link';
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className='bg-gray-900 px-4'>
      <div className="max-w-4xl mx-auto px-4">
         <div className="flex items-center justify-between py-4">
-      <div className='text-white'>Logo</div>
       <div className='text-white'>
-      <Link href="/steaks">Steak</Link>
+        <Link href="/">
+        <Image
+                            src="/logo2.png"
+                            width={100}
+                            height={100}
+                            alt=""
+                        />
+        </Link>
       </div>
       <div className='text-white'>
-      <Link href="/pork">Pork</Link>
+      <Link href="/collection">Collection</Link>
       </div>
       <div className='text-white'>
-      <Link href="/seafood">Seafood</Link>
+      <Link href="/aboutus">About Us</Link>
       </div>
       </div>
       </div>
