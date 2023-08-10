357b5d786a22b97d74164f4b65b14c5c05d9832c
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-09 20:16:02 -0700

    Added spacing for header

--- app/(products)/steaks/page.jsx
+++ app/(products)/steaks/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className='text-center'>
+      <div className='text-center mt-4 mb-4'>
         <h1 className='text-2xl font-semibold text-blue-900'>Steaks</h1>
       </div>
     </div>
